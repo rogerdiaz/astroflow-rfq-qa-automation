b8217231287d0dcf71d6b8d9de38c224a96ed382
Roger Diaz <dia12176>
2026-09-07 23:19:21 -0600

    Add ESLint and Prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['node_modules/', 'playwright-report/', 'test-results/', 'dist/', 'eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  eslintConfigPrettier,
+);
