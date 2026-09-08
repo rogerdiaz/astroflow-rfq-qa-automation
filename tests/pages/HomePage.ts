20b386bc14667a863368bd6f85374dcc3049fed1
Roger Diaz <dia12176>
2026-09-08 00:09:13 -0600

    Fix the RFQ Link to select the first element found

--- tests/pages/HomePage.ts
+++ tests/pages/HomePage.ts
@@ -2,7 +2,9 @@ import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class HomePage extends BasePage {
-  private readonly rfqLink = this.page.getByRole('link', { name: 'Request a Quote', exact: true });
+  private readonly rfqLink = this.page
+    .getByRole('link', { name: 'Request a Quote', exact: true })
+    .first();
 
   constructor(page: Page) {
     super(page);
